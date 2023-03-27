704296e5e72ecf1aad2bdaf3ec4f9f72e1d30d1c
george-tr6 <george.tr6@gmail.com>
2023-03-26 17:25:38 -0700

    added Navbar and Home components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'App Component';
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
