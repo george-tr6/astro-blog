1e0989db07428f1071a7a471e8b1c3ef4538d628
george-tr6 <george.tr6@gmail.com>
2023-04-06 19:16:54 -0700

    added set state fucntion to manage click and re-render

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = 'mario'
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log('hello ninjas', e);
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name, e.target);
+    // }
 
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={ (e) => {
+            {/* <button onClick={ (e) => {
                 handleClickAgain('mario', e)
-            }}>Click Me Again</button>
+            }}>Click Me Again</button> */}
         </div>
      );
 }
