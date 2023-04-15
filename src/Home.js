4e0c30240dbe1533a8e99c9c59d605cbaa81e39c
george-tr6 <george.tr6@gmail.com>
2023-04-14 19:14:08 -0700

    added pending message to display while data is loading

--- src/Home.js
+++ src/Home.js
@@ -3,27 +3,31 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     // const [name, setName] = useState('mario');
 
 
+// added setTimeout to simulate real-world condition of loading data
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then((data) => {
-            // console.log(data);
-            setBlogs(data);
-        })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then((data) => {
+                // console.log(data);
+                setBlogs(data);
+                setIsPending(false);
+            });
+        }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-            
-            
         </div>
      );
 }
